728ba0354f7a7c9b71f394eb118b723dc326185f
Joao Moreno <mail@joaomoreno.com>
2015-03-09 16:07:13 +0100

    10 slides

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static('public'));
+app.use('/dist', express.static('bower_components'));
 
 var server = app.listen(3001, function () {
 	console.log('http://localhost:3001/');
