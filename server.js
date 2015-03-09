7ca83b40c253d1a91cfb566e484611aef58b386b
Joao Moreno <mail@joaomoreno.com>
2015-03-09 14:51:45 +0100

    server

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+var server = app.listen(3001, function () {
+	console.log('http://localhost:3001/');
+});
