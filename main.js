946ba1c1356018aa9eafa5bd1a54c834c45ae4fb
Johannes Rieken <jrieken@microsoft.com>
2015-05-01 12:42:38 +0200

    wave

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ Reveal.initialize({
 	transition: 'slide',
 	dependencies: [
 		{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'reveal.js/plugin/notes/notes.js', async: true }
+		{ src: 'reveal.js/plugin/notes/notes.js', async: true },
+		{ src: 'reveal.js/plugin/wave/webcam-swiper-0.1.js'}, { src: 'plugin/wave/client.js'}
 	]
 });
