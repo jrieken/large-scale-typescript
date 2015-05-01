52ab05c6c8f0b057fd5f58a9f958795f23b36863
Johannes Rieken <jrieken@microsoft.com>
2015-05-01 12:48:48 +0200

    more fixing

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ Reveal.initialize({
 	dependencies: [
 		{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
 		{ src: 'reveal.js/plugin/notes/notes.js', async: true },
-		{ src: 'reveal.js/plugin/wave/webcam-swiper-0.1.js'}, { src: 'plugin/wave/client.js'}
+		{ src: 'reveal.js/plugin/wave/webcam-swiper-0.1.js'}, { src: 'reveal.js/plugin/wave/client.js'}
 	]
 });
