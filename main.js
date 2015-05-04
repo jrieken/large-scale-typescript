30a798f4d7068a38468881c5ff18390f8c7514df
Johannes Rieken <jrieken@microsoft.com>
2015-05-04 16:57:22 +0200

    make wave optional

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
+var dependencies = [
+	{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function () { return !!document.querySelector('pre code'); }, callback: function () { hljs.initHighlightingOnLoad(); } },
+	{ src: 'reveal.js/plugin/notes/notes.js', async: true }
+];
+
+if(window.location.search === '?wave') {
+	dependencies.push({ src: 'reveal.js/plugin/wave/webcam-swiper-0.1.js' });
+	dependencies.push({ src: 'reveal.js/plugin/wave/client.js' });
+}
+
 /* global Reveal */
 Reveal.initialize({
 	controls: false,
 	progress: false,
 	transition: 'slide',
-	dependencies: [
-		{ src: 'reveal.js/plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
-		{ src: 'reveal.js/plugin/notes/notes.js', async: true },
-		{ src: 'reveal.js/plugin/wave/webcam-swiper-0.1.js'}, { src: 'reveal.js/plugin/wave/client.js'}
-	]
+	dependencies: dependencies
 });
